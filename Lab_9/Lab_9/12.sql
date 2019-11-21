1100c28ad5d79fa9541589be341912e324444071
DongJun Cho (DJ) <dongjun819@gmail.com>
2019-11-20 20:17:47 -0800

    Update 12.sql

--- Lab_9/Lab_9/12.sql
+++ Lab_9/Lab_9/12.sql
@@ -1,4 +1,3 @@
-SELECT r_name, MAX(s_acctbal)
-FROM nation, region, Q2
-WHERE s_nationkey=n_nationkey AND n_regionkey=r_regionkey 
-GROUP BY r_name
+SELECT s_region, MAX(s_acctbal)
+FROM Q2 
+GROUP BY s_region
