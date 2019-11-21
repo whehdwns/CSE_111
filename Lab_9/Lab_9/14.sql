7cba935f0a68e564448f24509bc945cd204719b9
DongJun Cho (DJ) <dongjun819@gmail.com>
2019-11-20 20:18:18 -0800

    Update 14.sql

--- Lab_9/Lab_9/14.sql
+++ Lab_9/Lab_9/14.sql
@@ -1,7 +1,4 @@
---n1,r1 for supplying region
---n2,r2 for customer region
-SELECT r1.r_name, r2.r_name, CAST(SUM(l_extendedprice) AS INT)
-FROM region r1, region r2, nation n1,nation n2, lineitem, Q2, Q1, orders
-WHERE l_suppkey=s_suppkey AND s_nationkey=n1.n_nationkey AND n1.n_regionkey=r1.r_regionkey
-AND l_orderkey=o_orderkey AND o_custkey=c_custkey AND c_nationkey=n2.n_nationkey AND n2.n_regionkey = r2.r_regionkey
-GROUP BY r1.r_name, r2.r_name
+SELECT s_region, c_region, CAST(SUM(l_extendedprice) AS INT)
+FROM Q1, Q2, lineitem, orders
+WHERE l_suppkey=s_suppkey AND l_orderkey = o_orderkey AND o_custkey =c_custkey
+GROUP BY s_region, c_region
