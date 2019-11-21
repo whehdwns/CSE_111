4d0502e866bc7d8e0ffc2f83ccd7ad919e5ef626
DongJun Cho (DJ) <dongjun819@gmail.com>
2019-11-20 20:10:35 -0800

    Update 1.sql

--- Lab_9/Lab_9/1.sql
+++ Lab_9/Lab_9/1.sql
@@ -1,4 +1,4 @@
-SELECT c_name, ROUND(SUM(o_totalprice),2), n_name
-FROM Q1, nation, orders
-WHERE c_nationkey=n_nationkey AND n_name='FRANCE' AND o_custkey=c_custkey
+SELECT c_name, ROUND(SUM(o_totalprice),2), c_nation
+FROM Q1, orders
+WHERE c_nation='FRANCE' AND o_custkey=c_custkey
 GROUP BY c_custkey
