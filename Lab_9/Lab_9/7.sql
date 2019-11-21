b55409049e02296ae5877bc6a24a25528377d3ef
DongJun Cho (DJ) <dongjun819@gmail.com>
2019-11-20 20:14:08 -0800

    Update 7.sql

--- Lab_9/Lab_9/7.sql
+++ Lab_9/Lab_9/7.sql
@@ -1,4 +1,4 @@
-SELECT n_name, o_orderstatus, COUNT(o_orderkey)
-FROM nation, Q5, region, Q1
-WHERE o_custkey=c_custkey AND c_nationkey=n_nationkey AND n_regionkey=r_regionkey AND r_name='ASIA'
-GROUP BY n_name, o_orderstatus
+SELECT c_nation, o_orderstatus, COUNT(o_orderkey)
+FROM Q1, Q5
+WHERE o_custkey=c_custkey AND c_region='ASIA'
+GROUP BY c_nation, o_orderstatus
